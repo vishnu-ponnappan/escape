8a9970a0fdb99905e2a7716bd5b9766cd6e27829
Vishnu Ponnappan <vishnu.ponnappan@oracle.com>
2022-11-04 18:35:14 +0530

    changes

--- JS/code.js
+++ JS/code.js
@@ -1,1 +1,13 @@
-console.log("test")
+//Reverse a String;
+function reverseString(str){
+    var newString = "";
+    for(let i = str.length -1 ; i >= 0; i--){
+        newString += str[i];
+    }
+    return newString;
+};
+
+var str = "Vishnu";
+console.log(reverseString(str));
+
+
